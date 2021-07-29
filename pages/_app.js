955546f27ab9bc23ea42bb0a637f1bad8ef2c4d5
Zack Krida <zackkrida@pm.me>
2021-07-29 12:39:30 -0400

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { createClient } from "@liveblocks/client";
+import { LiveblocksProvider } from "@liveblocks/react";
+
+// Create a liveblocks client
+const client = createClient({
+  authEndpoint: "/api/auth",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    /**
+     * Add a LiveblocksProvider at the root of your app
+     * to be able to use liveblocks react hooks in your components
+     **/
+    <LiveblocksProvider client={client}>
+      <Component {...pageProps} />
+    </LiveblocksProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
